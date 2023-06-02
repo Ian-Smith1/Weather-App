a53e7cc8b4befece658e257b0c41f6bea1e2ee37
Ian-Smith1 <ianwsmith01@gmail.com>
2023-06-01 18:13:00 -0600

    added more to JS file, nearly done

--- assets/js/script.js
+++ assets/js/script.js
@@ -47,4 +47,88 @@ function searchHistory(city) {
 function getWeather(city) {
   var apiCoordsUrl =
     openWeatherCoordsUrl + city + "&appid=" + openWeatherApiKey;
+  fetch(apiCoordsUrl).then(function (coordResponse) {
+    if (coordResponse.ok) {
+      coordResponse.JSON().then(function (data) {
+        var cityLat = data.coord.lat;
+        var cityLong = data.coord.lon;
+        var apiOneCall =
+          apiUrl +
+          cityLat +
+          "&lon=" +
+          cityLong +
+          "&appid" +
+          openWeatherApiKey +
+          "&units=imperial";
+        fetch(apiOneCall).then(function (weatherAnswer) {
+          if (weatherAnswer.ok) {
+            weatherAnswer.JSON().then(function (weatherData) {
+              var currentWeather = $("<div>").attr({
+                id: "currentWeather",
+              });
+              var currentWeatherHeader = $("<h2>").text(city + currentDay);
+              var currentWeatherList = $("<ul>");
+
+              var currentWeatherDetails = [
+                "Temp: " + weatherData.current.temp + " °F",
+                "Wind: " + weatherData.current.wind_speed + " MPH",
+                "Humidity: " + weatherData.current.humidity + "%",
+                "UV Index: " + weatherData.current.uvi,
+              ];
+              for (var i = 0; i < currentWeatherDetails.length; i++) {
+                if (
+                  currentWeatherDetails[i] ===
+                  "UV Index: " + weatherData.current.uvi
+                ) {
+                  var currentWeatherItem = $("<li>").text("UV Index: ");
+                  currentWeatherList.append(currentWeatherItem);
+                  var uvItem = $("<span>").text(weatherData.current.uvi);
+                  if (uvItem.text() <= 2) {
+                    uvItem.addClass("favorable");
+                  } else if (uvItem.text() > 2 && uvItem.text() <= 7) {
+                    uvItem.addClass("moderate");
+                  } else {
+                    uvItem.addClass("severe");
+                  }
+                  currentWeatherItem.append(uvItem);
+                } else {
+                  var currentWeatherItem = $("<li>").text(
+                    currentWeatherDetails[i]
+                  );
+                  currentWeatherList.append(currentWeatherItem);
+                }
+              }
+              $("#fiveDay").before(currentWeather);
+              currentWeather.append(currentWeatherHeader);
+              currentWeather.append(currentWeatherList);
+
+              var fiveDayHeader = $("<h2>").text("5 Day Forcast:").attr({
+                id: "five-day-header",
+              });
+
+              var fiveDayArray = [];
+              for (var i = 0; i < 5; i++) {
+                let forcastDay = moment()
+                  .add(i + 1, "days")
+                  .format("M/DD/YYYY");
+                fiveDayArray.push(forcastDay);
+              }
+              for (var i = 0; i < fiveDayArray.length; i++) {
+                var cardDiv = $("<div>").addClass("card-body");
+                var cardTitle = $("<h3>")
+                  .addClass("card-title")
+                  .text(fiveDayArray);
+                var currentWeatherDetails = [
+                  "Temp: " + weatherData.current.temp + " °F",
+                  "Wind: " + weatherData.current.wind_speed + " MPH",
+                  "Humidity: " + weatherData.current.humidity + "%",
+                  "UV Index: " + weatherData.current.uvi,
+                ];
+              }
+            });
+          }
+        });
+      });
+    }
+  });
 }
